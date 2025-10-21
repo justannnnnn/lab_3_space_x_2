28f81535fe7c044081b78673c514dfc3c1326da3
justannn <kiritseva@sfedu.ru>
2025-10-21 17:10:27 +0300

    WIP 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { SpaceX } from "./api/spacex.js";
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import * as d3 from "d3";
 import * as Geo from "./geo.json";
 
 document.addEventListener("DOMContentLoaded", setup);
